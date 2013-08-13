8a8ec40cc763c9f439630d4a9ff26c6dc7a8ca1a
Steven Reynolds <steven.lamar.reynolds@gmail.com>
2013-08-12 22:18:23 -0500

    Added scala mutable hash map

--- src/net/slreynolds/ds/IntScalaMutableHashMapTiming.java
+++ src/net/slreynolds/ds/IntScalaMutableHashMapTiming.java
@@ -2,34 +2,35 @@ package net.slreynolds.ds;
 
 
 import scala.Tuple2;
-import scala.collection.immutable.HashMap;
+import scala.collection.mutable.HashMap;
+import scala.collection.mutable.Map;
 
 
-public class IntScalaHashMapTiming extends AbstractTiming<HashMap, HashMap> {
+public class IntScalaMutableHashMapTiming extends AbstractTiming<HashMap, HashMap> {
 
 
     @Override
     protected Result<HashMap> doWork(HashMap map) {
-        
+        Map lmap = map;
         final Tuple2[] vals = IntMapTimingParameters.valuesToInsert;
         final int n = IntMapTimingParameters.numToInsert;
         for (int i = 0; i < n; i++) {
-            map = map.$plus(vals[i]);
+            lmap = lmap.$plus(vals[i]);
         }
-        return new Result(map,map.size());
+        return new Result(lmap,lmap.size());
     }
     
     @Override
     protected HashMap setUp() {
         HashMap baseHMap = new HashMap();
         for (int i = 0; i < IntMapTimingParameters.initialSize; i++) {
-            baseHMap = baseHMap.$plus(new Tuple2(i,new SomeValue(i)));
+            baseHMap = (HashMap)baseHMap.$plus(new Tuple2(i,new SomeValue(i)));
         }
         return baseHMap;
     }
     
     public static void main(String[] args) { 
-        new IntScalaHashMapTiming().run("Int Scala HashMap");
+        new IntScalaMutableHashMapTiming().run("Int Scala mutable HashMap");
     }
 
 
