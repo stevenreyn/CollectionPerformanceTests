3757e9e6a5d96885fad504efcdbddc287661f408
Steven Reynolds <steven.lamar.reynolds@gmail.com>
2013-08-28 20:14:48 -0500

    Randomized the keys

--- src/net/slreynolds/ds/IntMapSource.scala
+++ src/net/slreynolds/ds/IntMapSource.scala
@@ -1,5 +1,7 @@
 package net.slreynolds.ds
 
+import scala.util.Random
+
 object IntMapSource {
   
     def initialSize:Int = 100000;
@@ -8,10 +10,11 @@ object IntMapSource {
   	val valuesToInsert: Array[Tuple2[Int,SomeValue]] = makeValues(numToInsert)
 	
 	private def makeValues(n: Int) : Array[Tuple2[Int,SomeValue]] = {
+  	  val rand = new Random(71234)
 	  val valsToIns:  Array[Tuple2[Int,SomeValue]] = new Array(n)
 	  for (i <- 0 to n-1) {
-            val j = i + initialSize;
-            valsToIns(i) = new Tuple2(j,new SomeValue(j));
+            val key = rand.nextInt(20*(n+initialSize))
+            valsToIns(i) = new Tuple2(key,new SomeValue(key));
         }
 	  valsToIns
 	}
