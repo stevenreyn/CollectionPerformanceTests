abf9fe7069d0bde481cc27c771c8318ed35b55c3
Steven Reynolds <steven.lamar.reynolds@gmail.com>
2013-08-28 21:10:11 -0500

    Added some keys to lookup

--- src/net/slreynolds/ds/IntMapSource.scala
+++ src/net/slreynolds/ds/IntMapSource.scala
@@ -8,6 +8,7 @@ object IntMapSource {
     def numToInsert:Int = 100000;
     
   	val valuesToInsert: Array[Tuple2[Int,SomeValue]] = makeValues(numToInsert)
+  	val keysToLookUp: Array[Int] = makeKeys(initialSize)
 	
 	private def makeValues(n: Int) : Array[Tuple2[Int,SomeValue]] = {
   	  val rand = new Random(71234)
@@ -15,8 +16,16 @@ object IntMapSource {
 	  for (i <- 0 to n-1) {
             val key = rand.nextInt(20*(n+initialSize))
             valsToIns(i) = new Tuple2(key,new SomeValue(key));
-        }
+      }
 	  valsToIns
 	}
 
+  	private def makeKeys(n: Int) : Array[Int] = {
+  	  val rand = new Random(65432)
+  	  val keys:  Array[Int] = new Array(n)
+	  for (i <- 0 to n-1) {
+            keys(i) = rand.nextInt(initialSize)
+      }
+	  keys
+  	}
 }
